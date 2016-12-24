3f4d83f956d1e78ffd2980f806ec44d904fec91d
Emmanuel Thomas <titbabthomas@gmail.com>
2016-12-24 12:52:19 -0500

    Use rspec instead of test

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,17 @@ module MyMoneyNew
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    # Load modules
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+      g.fixture_replacement :factory_girl
+      g.stylesheets false
+    end
   end
 end
